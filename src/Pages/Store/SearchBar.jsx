ce0c812f2a8879d8292a57b8c4102c4eb90af33b
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-07-01 02:47:04 +0530

    made adjustments on css of search bar + added a react icon

--- src/Pages/Store/SearchBar.jsx
+++ src/Pages/Store/SearchBar.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { IoIosSearch } from "react-icons/io";
 
 const SearchBar = ({ searchQuery, setSearchQuery }) => {
   return (
     <div className="search-bar">
-      <input 
-        type="text" 
-        value={searchQuery} 
-        onChange={(e) => setSearchQuery(e.target.value)} 
-        placeholder="Search products..."
-      />
+      <div className="search-bar-container">
+        <input 
+          type="text" 
+          value={searchQuery} 
+          onChange={(e) => setSearchQuery(e.target.value)} 
+          placeholder="Search products..." 
+        />
+        <IoIosSearch className="search-icon" />
+      </div>
     </div>
   );
 };
