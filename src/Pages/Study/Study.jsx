9f962ada8d4997221a102d9636a9e87bc3cce760
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-07-02 02:28:12 +0530

    made a proper bento grid layout

--- src/Pages/Study/Study.jsx
+++ src/Pages/Study/Study.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
-import './Study.css';  // Assuming you have CSS for styling
+import './Study.css';  
+import BentoGrid from './BentoGrid/BentoGrid';
 
-// Import images
-import img1 from './Images/img1.jpg';
-import img2 from './Images/img2.jpg';
-import img3 from './Images/img3.jpg';
+// import img1 from './Images/img1.jpg';
+// import img2 from './Images/img2.jpg';
+// import img3 from './Images/img3.jpg';
 
 const Study = () => {
   return (
-    <div className="bento-grid">
-      <div className="bento-column">
-        <div className="bento-item">
-          <img src={img1} alt="Item 1" />
-          <h3>Item 1</h3>
-          <p>Description of item 1</p>
-        </div>
-        <div className="bento-item">
-          <img src={img2} alt="Item 2" />
-          <h3>Item 2</h3>
-          <p>Description of item 2</p>
-        </div>
-      </div>
-      <div className="bento-item bento-item-large">
-        <img src={img3} alt="Item 3" />
-        <h3>Item 3</h3>
-        <p>Description of item 3</p>
-      </div>
-      {/* Add more items as needed */}
+    <div>
+        <BentoGrid />
     </div>
+    // <div className="bento-grid">
+    //   <div className="bento-column">
+    //     <div className="bento-item">
+    //       <img src={img1} alt="Item 1" />
+    //       <h3>Item 1</h3>
+    //       <p>Description of item 1</p>
+    //     </div>
+    //     <div className="bento-item">
+    //       <img src={img2} alt="Item 2" />
+    //       <h3>Item 2</h3>
+    //       <p>Description of item 2</p>
+    //     </div>
+    //   </div>
+    //   <div className="bento-item bento-item-large">
+    //     <img src={img3} alt="Item 3" />
+    //     <h3>Item 3</h3>
+    //     <p>Description of item 3</p>
+    //   </div>
+    //   {/* Add more items as needed */}
+    // </div>
   );
 }
 
