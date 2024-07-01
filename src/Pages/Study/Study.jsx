a02cbbeb0b514b422c017cd3cc552baf3e8669e8
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-07-02 01:38:01 +0530

    tried to add a bento grid to the study page

--- src/Pages/Study/Study.jsx
+++ src/Pages/Study/Study.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './Study.css';  // Assuming you have CSS for styling
+
+// Import images
+import img1 from './Images/img1.jpg';
+import img2 from './Images/img2.jpg';
+import img3 from './Images/img3.jpg';
 
 const Study = () => {
   return (
-    <div>Study</div>
-  )
+    <div className="bento-grid">
+      <div className="bento-column">
+        <div className="bento-item">
+          <img src={img1} alt="Item 1" />
+          <h3>Item 1</h3>
+          <p>Description of item 1</p>
+        </div>
+        <div className="bento-item">
+          <img src={img2} alt="Item 2" />
+          <h3>Item 2</h3>
+          <p>Description of item 2</p>
+        </div>
+      </div>
+      <div className="bento-item bento-item-large">
+        <img src={img3} alt="Item 3" />
+        <h3>Item 3</h3>
+        <p>Description of item 3</p>
+      </div>
+      {/* Add more items as needed */}
+    </div>
+  );
 }
 
 export default Study;
