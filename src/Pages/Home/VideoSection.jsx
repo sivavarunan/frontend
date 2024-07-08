45df29ab62330c41b5541059ca66ca1f35c2a291
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-07-08 23:49:48 +0530

    ..

--- src/Pages/Home/VideoSection.jsx
+++ src/Pages/Home/VideoSection.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import './VideoSection.css'; 
-import animation from './videoes/animation.mp4'
-import Typewriter from 'react-typewriter-effect';
+import animation from './videoes/animation.mp4';
 import { Fade } from 'react-awesome-reveal';
 
+const text = "The World is What We Imagine.. Enjoy every moment of it.";
 
 const VideoSection = () => {
   return (
     <div className="video-section">
-      <video autoPlay loop  className="video">
-        <source src={animation} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className="overlay">
-        <Fade direction="up" triggerOnce delay={100}>
-          <Typewriter
-            text="The World is What We Imagine..
-            Enjoy every moment of it."
-            typeSpeed={100}
-            startDelay={100}
-            cursorColor="white"
-          />
-        </Fade>
-      </div>
+      <Fade direction="up" triggerOnce delay={100}>
+        <video autoPlay loop className="video">
+          <source src={animation} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </Fade>
+      <motion.div
+        className="overlay"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        {text.split("").map((char, index) => (
+          <motion.span
+            key={index}
+            className="gradient-text"
+            initial={{ opacity: 0, translateY: 20 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            transition={{ duration: 0.8, delay: index * 0.1 }}
+          >
+            {char}
+          </motion.span>
+        ))}
+      </motion.div>
     </div>
   );
 };
