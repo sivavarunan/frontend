4a2002a5526aa597d0c638fe7455c550a700ea2d
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-07-18 16:50:00 +0530

    made a timer for section3 using use effect

--- src/Pages/Store/section3.jsx
+++ src/Pages/Store/section3.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import './section3.css';
 
@@ -13,45 +13,53 @@ const Section3 = () => {
     document.querySelector('.slide').prepend(items[items.length - 1]);
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleNextClick();
+    }, 6000); // Change image every 6 seconds
+
+    return () => clearInterval(interval); // Cleanup the interval on component unmount
+  }, []);
+
   return (
     <div className="containerx">
       <div className="slide">
-      <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/qCkd9jS/img1.jpg)', borderRadius: '20px' }}>
-      <div className="content">
+        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/qCkd9jS/img1.jpg)', borderRadius: '20px' }}>
+          <div className="content">
             <div className="name">Switzerland</div>
             <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
             <button>See More</button>
           </div>
         </div>
-        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/jrRb11q/img2.jpg)', borderRadius: '20px'  }}>
+        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/jrRb11q/img2.jpg)', borderRadius: '20px' }}>
           <div className="content">
             <div className="name">Finland</div>
             <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
             <button>See More</button>
           </div>
         </div>
-        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/NSwVv8D/img3.jpg)', borderRadius: '20px'  }}>
+        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/NSwVv8D/img3.jpg)', borderRadius: '20px' }}>
           <div className="content">
             <div className="name">Iceland</div>
             <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
             <button>See More</button>
           </div>
         </div>
-        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/Bq4Q0M8/img4.jpg)' ,borderRadius: '20px'  }}>
+        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/Bq4Q0M8/img4.jpg)', borderRadius: '20px' }}>
           <div className="content">
             <div className="name">Australia</div>
             <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
             <button>See More</button>
           </div>
         </div>
-        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/jTQfmTq/img5.jpg)', borderRadius: '20px'  }}>
+        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/jTQfmTq/img5.jpg)', borderRadius: '20px' }}>
           <div className="content">
             <div className="name">Netherland</div>
             <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
             <button>See More</button>
           </div>
         </div>
-        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/RNkk6L0/img6.jpg)', borderRadius: '20px'  }}>
+        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/RNkk6L0/img6.jpg)', borderRadius: '20px' }}>
           <div className="content">
             <div className="name">Ireland</div>
             <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
