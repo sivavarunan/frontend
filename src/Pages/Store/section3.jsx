3ccbf8bfb361f51ff227bb0cec9230b2b2c4cb40
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-07-18 16:14:03 +0530

    improving the store page

--- src/Pages/Store/section3.jsx
+++ src/Pages/Store/section3.jsx
@@ -16,42 +16,42 @@ const Section3 = () => {
   return (
     <div className="containerx">
       <div className="slide">
-        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/qCkd9jS/img1.jpg)' }}>
-          <div className="content">
+      <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/qCkd9jS/img1.jpg)', borderRadius: '20px' }}>
+      <div className="content">
             <div className="name">Switzerland</div>
             <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
             <button>See More</button>
           </div>
         </div>
-        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/jrRb11q/img2.jpg)' }}>
+        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/jrRb11q/img2.jpg)', borderRadius: '20px'  }}>
           <div className="content">
             <div className="name">Finland</div>
             <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
             <button>See More</button>
           </div>
         </div>
-        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/NSwVv8D/img3.jpg)' }}>
+        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/NSwVv8D/img3.jpg)', borderRadius: '20px'  }}>
           <div className="content">
             <div className="name">Iceland</div>
             <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
             <button>See More</button>
           </div>
         </div>
-        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/Bq4Q0M8/img4.jpg)' }}>
+        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/Bq4Q0M8/img4.jpg)' ,borderRadius: '20px'  }}>
           <div className="content">
             <div className="name">Australia</div>
             <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
             <button>See More</button>
           </div>
         </div>
-        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/jTQfmTq/img5.jpg)' }}>
+        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/jTQfmTq/img5.jpg)', borderRadius: '20px'  }}>
           <div className="content">
             <div className="name">Netherland</div>
             <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
             <button>See More</button>
           </div>
         </div>
-        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/RNkk6L0/img6.jpg)' }}>
+        <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/RNkk6L0/img6.jpg)', borderRadius: '20px'  }}>
           <div className="content">
             <div className="name">Ireland</div>
             <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
